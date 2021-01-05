0eb704d17ba2ab039bee58019266ab22dcdd025e
YuyaIida <k16005kk@aitech.ac.jp>
2021-01-06 00:25:16 +0900

    add: articleDetail component

---
+++ src/pages/articleDetail/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from 'src/components/molecules/Header'
+
+const ArticleDetail: React.FC = () => {
+  return (
+    <>
+      <Header />
+      ここに記事詳細がきます ここに記事詳細がきます ここに記事詳細がきます ここに記事詳細がきます ここに記事詳細がきます
+      ここに記事詳細がきます ここに記事詳細がきます
+    </>
+  )
+}
+
+export default ArticleDetail
